c1337664e1221343f7973943e01773899e299386
HooferDevelops <BatHun0214A@mischools.org>
2021-02-17 14:40:45 -0500

    code reformatting

--- components/EnhancedImage.jsx
+++ components/EnhancedImage.jsx
@@ -6,8 +6,70 @@ import { TextInput, SwitchItem, ButtonItem, Category, SliderInput } from '@vizal
 module.exports = class EnhancedImage extends React.PureComponent {
     constructor(props){
         super(props)
+        this.hooked = false;
+        this.magnifierSize = 150;
+        this.magnifierZoom = 3;
+        this.magnifier;
+
     }
 
+    settingsCheck(){
+        if (this.settings.get("circleLens", true)){
+            this.magnifier.classList.add("en-img-magnifier-round");
+            this.magnifier.classList.remove("en-img-magnifier-square");
+        } else {
+            this.magnifier.classList.remove("en-img-magnifier-round");
+            this.magnifier.classList.add("en-img-magnifier-square");
+        }
+        if (this.settings.get("smoothLens", true)){
+            this.magnifier.classList.add("en-img-magnifier-smooth");
+        } else {
+            this.magnifier.classList.remove("en-img-magnifier-smooth");
+        }
+        if (this.settings.get("antiAliasLens", false)){
+            this.magnifier.classList.remove("en-img-magnifier-pixel");
+        } else {
+            this.magnifier.classList.add("en-img-magnifier-pixel");
+        }
+        if (this.settings.get("borderLens", true)){
+            this.magnifier.classList.add("en-img-magnifier-border");
+        } else {
+            this.magnifier.classList.remove("en-img-magnifier-border");
+        }
+    }
+
+    moveEvent(data){
+        data = data || window.event;
+        var x = data.pageX - this.magnifier.parentElement.getBoundingClientRect().left - window.pageXOffset;
+        var y = data.pageY - this.magnifier.parentElement.getBoundingClientRect().top - window.pageYOffset;
+        x = Math.min(Math.max(x,0),this.props.width);
+        y = Math.min(Math.max(y,0),this.props.height);
+        console.log(y);
+        this.magnifier.style.left = `${x-this.magnifierSize/2}px`;
+        this.magnifier.style.top = `${y-this.magnifierSize/2}px`;
+        this.magnifier.style.backgroundPosition = `${-x*this.magnifierZoom+(this.magnifierSize/2)}px ${-y*this.magnifierZoom+(this.magnifierSize/2)}px`;
+        // some checks to see if settings have changed
+        this.settingsCheck();
+        // not inside of image
+        if (y <= 0 || data.pageY > this.magnifier.parentElement.getBoundingClientRect().top+this.props.height || x <= 0 || data.pageX > this.magnifier.parentElement.getBoundingClientRect().left+this.props.width){ 
+            if (this.props.settings.get("hideLens", true)){
+                this.magnifier.classList.add("en-img-magnifier-hide")
+            }
+        } else {
+            this.magnifier.classList.remove("en-img-magnifier-hide")
+        }
+        this.magnifier.style.backgroundSize  = `${this.props.width * this.magnifierZoom}px ${this.props.height * this.magnifierZoom}px`
+    }
+
+    scrollEvent(data){
+        data.preventDefault();
+        this.magnifierSize += 50 * Math.sign(data.deltaY) * -1;
+        this.magnifierSize = Math.min(Math.max(this.magnifierSize, 50),500);
+        this.magnifierZoom = this.magnifierSize / 50;
+        this.magnifier.style.width = `${this.magnifierSize}px`
+        this.magnifier.style.height = `${this.magnifierSize}px`
+        this.moveEvent(data);
+    }
 
     render() {
         return <>
@@ -15,58 +77,19 @@ module.exports = class EnhancedImage extends React.PureComponent {
                 <div style={{width:this.props.width,height:this.props.height}} className="en-img-wrapper imageWrapper-2p5ogY image-1tIMwV">
                     <div 
                     ref={(e)=>{
-                        if (!e)
+                        if (!e || this.hooked)
                             return;
 
-                        const width = this.props.width;
-                        const height = this.props.height;
-                        const settings = this.props.settings;
-                        function move(data){
-                            data = data || window.event;
-                            var x = data.pageX - e.parentElement.getBoundingClientRect().left - window.pageXOffset;
-                            var y = data.pageY - e.parentElement.getBoundingClientRect().top - window.pageYOffset;
-                            x = Math.min(Math.max(x,0),width);
-                            y = Math.min(Math.max(y,0),height);
-                            e.style.left = `${x-100}px`;
-                            e.style.top = `${y-100}px`;
-                            e.style.backgroundPosition = `${-x*3+100}px ${-y*3+100}px`;
-                            // some checks to see if settings have changed
-                            if (settings.get("circleLens", true)){
-                                e.classList.add("en-img-magnifier-round");
-                                e.classList.remove("en-img-magnifier-square");
-                            } else {
-                                e.classList.remove("en-img-magnifier-round");
-                                e.classList.add("en-img-magnifier-square");
-                            }
-                            if (settings.get("smoothLens", true)){
-                                e.classList.add("en-img-magnifier-smooth");
-                            } else {
-                                e.classList.remove("en-img-magnifier-smooth");
-                            }
-                            if (settings.get("antiAliasLens", false)){
-                                e.classList.remove("en-img-magnifier-pixel");
-                            } else {
-                                e.classList.add("en-img-magnifier-pixel");
-                            }
-                            if (settings.get("borderLens", true)){
-                                e.classList.add("en-img-magnifier-border");
-                            } else {
-                                e.classList.remove("en-img-magnifier-border");
-                            }
-                            // not inside of image
-                            if (y <= 0 || data.pageY > e.parentElement.getBoundingClientRect().top+height || x <= 0 || data.pageX > e.parentElement.getBoundingClientRect().left+width){ 
-                                if (settings.get("hideLens", true)){
-                                    e.classList.add("en-img-magnifier-hide")
-                                }
-                            } else {
-                                e.classList.remove("en-img-magnifier-hide")
-                            }
-                        }
-                        e.addEventListener("mousemove", move);
-                        e.parentElement.addEventListener("mousemove", move);
-                        e.style.backgroundImage = `url(${this.props.src})`
-                        e.style.backgroundSize  = `${this.props.width * 3}px ${this.props.height * 3}px`
-                    }} className="en-img-magnifier-round en-img-magnifier-hide" style={{backgroundRepeat:"no-repeat",position:"fixed",width:"200px",height:"200px",zIndex:"999"}}/>
+                        this.hooked = true;
+                        this.magnifier = e;
+
+                        e.addEventListener("wheel", event => this.scrollEvent(event));
+                        e.parentElement.addEventListener("wheel", event => this.scrollEvent(event));
+                        e.addEventListener("mousemove", event => this.moveEvent(event));
+                        e.parentElement.addEventListener("mousemove", event => this.moveEvent(event));
+
+                        e.style.backgroundImage = `url(${this.props.src})`;
+                    }} className="en-img-magnifier-round en-img-magnifier-hide" style={{backgroundRepeat:"no-repeat",position:"fixed",width:`150px`,height:`150px`,zIndex:"999"}}/>
                     <img className="en-img-view" src={this.props.src} style={{width:this.props.width,height:this.props.height}}/>
                 </div>
             </div>
